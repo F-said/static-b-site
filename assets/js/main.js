3159769a21f0f56b30a7777590cf224457a26e99
F-said <f.saidmuratov@yahoo.com>
2022-03-06 14:25:52 -0500

    Updated with russian button

--- assets/js/main.js
+++ assets/js/main.js
@@ -350,3 +350,14 @@
 	});
 
 })(jQuery);
+
+var flags = document.getElementsByClassName('flag_link');
+
+Array.prototype.forEach.call(flags, function(e){
+  e.addEventListener('click', function(){
+    var lang = e.getAttribute('data-lang'); 
+    var languageSelect = document.querySelector("select.goog-te-combo");
+    languageSelect.value = lang; 
+    languageSelect.dispatchEvent(new Event("change"));
+  }); 
+});
